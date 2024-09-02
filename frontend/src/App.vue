eac9992a1569a4b920ec898acf30f3e4c0db2b5f
jozwik12 <78445405+jozwik12@users.noreply.github.com>
2024-09-02 19:51:23 +0200

    change folder names

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,14 +4,10 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
-    </div>
   </header>
 
   <RouterView />
