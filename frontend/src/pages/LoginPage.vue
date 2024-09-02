86b99b6989e0412ee889d2edd31cfd5074d8802f
jozwik12 <78445405+jozwik12@users.noreply.github.com>
2024-09-02 20:07:52 +0200

    add login page mock

--- frontend/src/pages/LoginPage.vue
+++ frontend/src/pages/LoginPage.vue
@@ -1,4 +1,23 @@
 <template>
   <h1>This is a login page</h1>
-  <div></div>
+  <div id="loginMock">
+    <span>
+      <input placeholder="name" />
+    </span>
+    <br />
+    <span>
+      <input placeholder="password" />
+    </span>
+  </div>
+  <button @click="handleClick">go to home page</button>
 </template>
+
+<script>
+export default {
+  methods: {
+    handleClick() {
+      this.$router.push('/')
+    }
+  }
+}
+</script>
